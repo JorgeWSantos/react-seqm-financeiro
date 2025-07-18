361b70e48a40b62e489dcc65b7e16bb8658b6926
JorgeWS <jorgews.dev@gmail.com>
2025-07-18 03:10:59 -0300

    feat: 🎸 Melhorias na impressão

--- src/pages/EventSummary/PrintArea.styles.ts
+++ src/pages/EventSummary/PrintArea.styles.ts
@@ -11,7 +11,7 @@ export const PrintAreaWrapper = styled.div`
   background-color: white;
 `;
 
-export const PrintAreaContainer = styled.div`
+export const PrintAreaHeader = styled.div`
   margin: 0 auto;
   width: 100%;
   background-color: white;
@@ -71,5 +71,53 @@ export const IconWrapper = styled.div`
 
   svg {
     margin-bottom: -8pt;
+  }
+`;
+
+// --------content
+
+export const DivWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  /* margin-top: 8pt; */
+`;
+
+export const DivTitle = styled.div`
+  margin-bottom: 12pt;
+  p {
+    font-size: 14pt;
+    font-weight: bold;
+    margin: 0;
+  }
+`;
+
+export const DivCardsRow = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 6pt;
+`;
+
+export const DivCard = styled.div`
+  width: 100%;
+  height: 40pt;
+  background-color: #f0f0f0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  padding-bottom: 10pt;
+
+  p {
+    font-size: 8pt;
+  }
+
+  p + p {
+    font-size: 10pt;
+    font-weight: 600;
   }
 `;
--- src/pages/EventSummary/PrintArea.tsx
+++ src/pages/EventSummary/PrintArea.tsx
@@ -2,38 +2,72 @@ import React from 'react';
 import SVG from './logo-seqm.svg';
 import {
   PrintAreaWrapper,
-  PrintAreaContainer,
+  PrintAreaHeader,
   LogoWrapper,
   DetailsWrapper,
   DetailsInfo,
   IconWrapper,
   EventName,
+  DivWrapper,
+  DivTitle,
+  DivCardsRow,
+  DivCard,
 } from './PrintArea.styles';
 import { RanchSortingIconSEQM } from '@abqm-ds/icons';
+
+const PrintHeader: React.FC = () => {
+  return (
+    <PrintAreaHeader>
+      <LogoWrapper>
+        <img src={SVG} width={'170px'} alt="SEQM Logo" />
+      </LogoWrapper>
+
+      <DetailsWrapper>
+        <EventName>NOME DO EVENTO</EventName>
+
+        <DetailsInfo>
+          <p>Nome do responsável do evento</p>
+          <p>Cidade, UF</p>
+          <p>00/00/0000 - 00/00/0000</p>
+        </DetailsInfo>
+      </DetailsWrapper>
+
+      <IconWrapper>
+        <RanchSortingIconSEQM width={'40pt'} height={'40pt'} fill="#424242" />
+        <p style={{ marginTop: '-6pt' }}>Ranch Sorting</p>
+      </IconWrapper>
+    </PrintAreaHeader>
+  );
+};
 
 const PrintArea: React.FC = () => {
   return (
     <PrintAreaWrapper id="print-area">
-      <PrintAreaContainer>
-        <LogoWrapper>
-          <img src={SVG} width={'170px'} />
-        </LogoWrapper>
+      <PrintHeader />
 
-        <DetailsWrapper>
-          <EventName>NOME DO EVENTO</EventName>
-
-          <DetailsInfo>
-            <p>Nome do responsável do evento</p>
-            <p>Cidade, UF</p>
-            <p>00/00/0000 - 00/00/0000</p>
-          </DetailsInfo>
-        </DetailsWrapper>
-
-        <IconWrapper>
-          <RanchSortingIconSEQM width={'40pt'} height={'40pt'} fill="#424242" />
-          <p style={{ marginTop: '-6pt' }}>Ranch Sorting</p>
-        </IconWrapper>
-      </PrintAreaContainer>
+      <DivWrapper>
+        <DivTitle>
+          <p>RESUMO DA MODALIDADE</p>
+        </DivTitle>
+        <DivCardsRow>
+          <DivCard>
+            <p>DATA DO EVENTO</p>
+            <p>00/00/0000</p>
+          </DivCard>
+          <DivCard>
+            <p>DATA DO EVENTO</p>
+            <p>00/00/0000</p>
+          </DivCard>
+          <DivCard>
+            <p>DATA DO EVENTO</p>
+            <p>00/00/0000</p>
+          </DivCard>
+          <DivCard>
+            <p>DATA DO EVENTO</p>
+            <p>00/00/0000</p>
+          </DivCard>
+        </DivCardsRow>
+      </DivWrapper>
     </PrintAreaWrapper>
   );
 };
