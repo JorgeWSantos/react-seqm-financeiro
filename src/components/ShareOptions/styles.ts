bc5e82e37ed314d0653c3412d2f7d057581cfd0a
JorgeWS <jorgews.dev@gmail.com>
2025-07-21 04:40:47 -0300

    feat: 🎸 Melhoria no ShareOptions, permitindo que ele funcione corretamente no mobile e tablet

--- src/components/ShareOptions/styles.ts
+++ src/components/ShareOptions/styles.ts
@@ -1,27 +1,44 @@
+import { breakpointsPx, radii, space } from '@abqm-ds/tokens';
 import styled from 'styled-components';
 
 export const ShareOptionsContainer = styled.div`
   position: absolute;
-  right: 30px;
-  top: 64px;
+  right: 1.875rem;
+  top: 4rem;
   z-index: 9999;
   background: white;
-  border: 1px solid #eee;
-  border-radius: 8px;
-  padding: 16px;
+  border: ${radii.px} solid #eee;
+  border-radius: ${space[2]};
+  padding: ${space[4]};
   display: flex;
-  gap: 12px;
+  gap: ${space[3]};
   align-items: center;
 
   &::after {
     content: '';
     position: absolute;
-    top: -8px;
-    right: 40px;
-    border-width: 0 8px 8px 8px;
+    top: -0.5rem;
+    right: 2.5rem;
+    border-width: 0 ${radii.md} ${radii.md} ${radii.md};
     border-style: solid;
     border-color: transparent transparent white transparent;
     display: block;
+  }
+
+  @media (max-width: ${breakpointsPx.lg}) {
+    top: unset;
+    bottom: 4.5rem;
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 4rem;
+      right: 2.5rem;
+      border-width: ${radii.md} ${radii.md} 0 ${radii.md};
+      border-style: solid;
+      border-color: white transparent transparent transparent;
+      display: block;
+    }
   }
 `;
 
@@ -30,8 +47,8 @@ export const IconButton = styled.button`
   border: none;
   padding: 0;
   border-radius: 50%;
-  width: 32px;
-  height: 32px;
+  width: 2rem;
+  height: 2rem;
   display: flex;
   align-items: center;
   justify-content: center;
