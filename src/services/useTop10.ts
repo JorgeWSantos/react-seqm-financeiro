a82518195f231dbb99893fc2f5affb505e726170
JorgeWS <jorgews.dev@gmail.com>
2025-07-27 19:06:41 -0300

    fix: 🐛 Corrigirá as importações

--- src/services/useTop10.ts
+++ src/services/useTop10.ts
@@ -1,10 +1,7 @@
 import { Toast } from '@abqm-ds/react';
 import { useCallback } from 'react';
 import { apiResultados } from './api';
-import type {
-  Top10Response,
-  Top10ResponseData,
-} from '@src/pages/ModalityResults/types.api';
+import type { Top10Response, Top10ResponseData } from '@src/pages/Top10/types.api';
 
 export function useTop10() {
   const getTop10 = useCallback(
