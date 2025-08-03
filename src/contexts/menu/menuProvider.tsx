9ac3142115fb8f315bb0c4ed317f9ea27601658f
JorgeWS <jorgews.dev@gmail.com>
2025-08-03 15:28:13 -0300

    feat: 🎸 Melhoria no menu

--- src/contexts/menu/menuProvider.tsx
+++ src/contexts/menu/menuProvider.tsx
@@ -1,32 +1,25 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { MenuContext } from './menuContext';
 import { useGeneralService, type MenuResponseObj } from '@src/services/useGeneralService';
-import type { MenuType } from '@abqm-ds/react';
 
 export const MenuProvider = ({ children }: { children: React.ReactNode }) => {
-  const [menu, setMenu] = useState<MenuType | []>([]);
-
   const { getMenu } = useGeneralService();
 
   const onGetMenu = useCallback(async () => {
-    const loadMenu = async () => {
-      const menu: MenuResponseObj = await getMenu();
-      setMenu(menu.list_menu);
-    };
-    loadMenu();
+    const menu: MenuResponseObj = await getMenu();
+    return menu.list_menu;
   }, [getMenu]);
 
-  useQuery({
+  const { data: menu } = useQuery({
     queryKey: ['menu'],
     queryFn: onGetMenu,
-    // enabled: true,
   });
 
   return (
     <MenuContext.Provider
       value={{
-        menu,
+        menu: menu ?? [],
       }}
     >
       {children}
