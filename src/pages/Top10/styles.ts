f902ed1cd96778e225f43fb123191c2117df0dc9
JorgeWS <jorgews.dev@gmail.com>
2025-10-04 19:34:01 -0300

    feat: 🎸 Melhorias na estilização do top 10

--- src/pages/Top10/styles.ts
+++ src/pages/Top10/styles.ts
@@ -1,4 +1,3 @@
-import { colors } from '@abqm-ds/tokens';
 import styled from 'styled-components';
 
 export const ContainerMain = styled.div`
@@ -10,36 +9,9 @@ export const ContainerMobileMain = styled.div`
   height: 100%;
 `;
 
-export const Scrollable = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow-y: auto;
-  overflow-x: auto;
-`;
-
-export const LoadingContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-export const NotFoundContainer = styled.div`
-  width: 100%;
-  padding: 1rem 0;
-`;
-
-export const DivTopMobile = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid ${colors.white25};
-  padding-bottom: 0.375rem;
-`;
-
-export const DivInfoCard = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  gap: 0.125rem;
+export const RemoveScrollableMobile = styled.div`
+  overflow-x: unset;
+  overflow-y: unset;
 `;
 
 export const EventHeader = styled.div`
