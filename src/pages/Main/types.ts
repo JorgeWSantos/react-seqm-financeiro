68c1962c4fb36c9efedd22594ca8980ff080cbc0
JorgeWS <jorgews.dev@gmail.com>
2025-12-01 04:03:19 -0300

    fix: 🐛 Corrigirá erros de tipagem

--- src/pages/Main/types.ts
+++
@@ -1,7 +0,0 @@
-import type { ClassificatoryData } from "@src/services/Classificatory/types.classificatory.api";
-
-export interface Tab {
-  tipo_etapa: string;
-  cartao_julgamento: string;
-  lista_classificacao?: ClassificatoryData[];
-}
